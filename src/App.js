1af374b7a60115bf4f6984c575bb53892abf8348
Moti Samadi <motahhareh.samadi@gmail.com>
2023-04-09 14:49:18 +0200

    React opean weather API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [weather, setWeather] = useState(null);
+    const city = "Munich";
+    const apiKey = "cd9b4c6a5a6761e1af9c0ebe7c0e74b7"
+
+    useEffect(() => {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setWeather(data);
+            })
+    }, [city, apiKey]);
+
+    return (
+        <div className="App">
+            {weather ? (
+                    <div>
+                        <h1>Weather in {weather.name}</h1>
+                        <p>Temperature: {weather.main.temp}°C</p>
+                        <p>Humidity: {weather.main.humidity}%</p>
+                        {/*<p>Weather: {weather.weather[0].description}</p>*/}
+                    </div>
+                ) :
+                (<p>Weather Data is loading...</p>)
+            }
+        </div>
+    );
 }
 
 export default App;
